2a2cf913d14df574259335ba7f629ce8125f3585
Sergey_v <sergei_v@flashphoner.com>
2026-02-18 15:15:03 +0300

    Fixed decrease stats

--- src/main/java/com/example/dnd_manager/info/version/AppInfo.java
+++ src/main/java/com/example/dnd_manager/info/version/AppInfo.java
@@ -7,7 +7,7 @@ package com.example.dnd_manager.info.version;
  */
 public final class AppInfo {
 
-    private static final String VERSION = "1.0.2";
+    private static final String VERSION = "1.0.3";
 
     private AppInfo() {
     }
